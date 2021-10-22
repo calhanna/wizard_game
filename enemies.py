56c5d784a597c9c74c34e494f6baf4ec437c65f4
calhanna <86635307+calhanna@users.noreply.github.com>
2021-10-22 19:29:27 +1300

    Updated enemy pathfinding + commenting
    - Updated enemy pathfinding for better avoidance and performance
    - Removed the pathfinding library 
    - Enemy sprites are temporarily replaced with red squares
    - Firing now follows arrow keys instead of mouse
    - More commenting

--- enemies.py
+++ enemies.py
@@ -1,83 +1,52 @@
-import pygame, pathfinding
-from pathfinding.core.diagonal_movement import DiagonalMovement
-from pathfinding.finder.a_star import AStarFinder
+import pygame, pathfinding, random
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y, image, matrix):
+    def __init__(self, x, y, image):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = image
-        self.matrix = matrix
         self.facing = 'right'
 
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
-        self.end = 0
+    def find_path(self, goal, matrix):
+        start = (int(self.rect.centery/32), int(self.rect.centerx/32))
+        end = (int(goal[1]/32), int(goal[0]/32))
 
-        self.path = [((self.rect.centerx + 32)/32, (self.rect.centery + 32)/32 )]
-
-    def find_path(self, player):
-        self.matrix.cleanup()
-        start = (int(self.rect.centerx / 32), int(self.rect.centery / 32))
-        start = self.matrix.node(start[0], start[1])
-
-        self.end = (int(player[0] / 32), int(player[1] / 32))
-        end = self.matrix.node(self.end[0], self.end[1])
-
-        finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-        path, runs = finder.find_path(start, end, self.matrix)
+        path = pathfinding.astar(matrix, start, end)
 
         return path
 
-    def move(self, player):
-        try:
-            goal = self.path[0]
-            goal = (goal[0] * 32, goal[1] * 32)
-        except IndexError:
-            return pygame.Vector2((0,0))
-
+    def move(self):
         move = pygame.Vector2((0,0))
 
-        if self.rect.center == goal:
-            self.path = self.find_path(player)
-            self.path.remove(self.path[0])
-            if len(self.path) > 0:
-                goal = self.path[0]
-                goal = (goal[0] * 32, goal[1] * 32)
-            else:
-                return pygame.Vector2((0,0))
+        goal = self.path[1]
 
-        if self.rect.centerx < goal[0]:
-            move[0] += 1
-            if self.facing == 'right':
-                self.image = pygame.transform.flip(self.image, True, False)
-                self.facing = 'left'
-        elif self.rect.centerx > goal[0]:
-            move[0] -= 1
-            if self.facing == 'left':
-                self.image = pygame.transform.flip(self.image, True, False)
-                self.facing = 'right'
-        
-        if self.rect.centery < goal[1]:
-            move[1] += 1
-        elif self.rect.centery > goal[1]:
-            move[1] -= 1
+        if (int(self.rect.centery / 32), int(self.rect.centerx / 32)) == goal:
+            return True
+        else:
+            if self.rect.centerx < goal[1] * 32: move[0] += 1
+            if self.rect.centerx > goal[1] * 32: move[0] -= 1
+            if self.rect.centery < goal[0] * 32: move[1] += 1
+            if self.rect.centery > goal[0] * 32: move[1] -= 1
 
-        return move
+            return move
 
 class Wolf(Enemy):
-    def __init__(self, x, y, matrix):
-        self.image = pygame.image.load('images/wolf/wolf_0.png')
-        Enemy.__init__(self, x, y, self.image, matrix)
+    def __init__(self, x, y):
+        #self.image = pygame.image.load('images/wolf/wolf_0.png')
+        self.image = pygame.surface.Surface((32,32))
+        self.image.fill((255,0,0))
+        Enemy.__init__(self, x, y, self.image)
 
-        self.anim_count = -1
-        self.tick = 0
+        #self.anim_count = -1
+        #self.tick = 0
 
-        self.animation = [self.image]
-        for i in range(0, 2):
-            s = str(i)
-            self.animation.append(pygame.image.load('images/wolf/wolf_' + s + '.png'))
+        #self.animation = [self.image]
+        #for i in range(0, 2):
+        #    s = str(i)
+        #    self.animation.append(pygame.image.load('images/wolf/wolf_' + s + '.png'))
 
     def cycle_anim(self):
         if self.tick >= 15:
--- main.py
+++ main.py
@@ -1,58 +1,71 @@
-import pygame, pytmx, pathfinding, math
-from pathfinding.core.grid import Grid
+import pygame, pytmx, math, random                  # Premade libraries
 
-import projectiles, enemies
+import projectiles, enemies, pathfinding            # Additional Files
 
 from player import Player
 
-pygame.init()
+pygame.init()                                       # Pygame initialisation
 
-screen = pygame.display.set_mode((640, 640))
+screen = pygame.display.set_mode((640, 640))        # Window Creation
 
-tick = 0
+tick = 0                                            # Runtime tracker
 clock = pygame.time.Clock()
 
 # Variable Definition
 background = pygame.image.load('images/grass_background.png')
-all_sprites = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()                 # Main sprite group. Handles drawing of everything except bullets.
 
 player = Player()
 
-fireball_cooldown = 0
-max_cooldown = 20
+fireball_cooldown = 0                               # Limits the speed at which the player can cast fireball. Incremented every tick.
+max_cooldown = 30                                   # The threshold of fireball_cooldown after which another fireball can be cast
 
-bullets = pygame.sprite.Group()
-all_enemies = pygame.sprite.Group()
+bullets = pygame.sprite.Group()                     # Handles drawing of bullets below other sprites
+all_enemies = pygame.sprite.Group()                 # Handles enemy behavior
 
 all_sprites.add(player)
 
-matrix = []
+matrix = []                                         # Generates map as 1s and 0s - 1 representing obstacles and 0 representing empty space
 for i in range(20):
     matrix.append([])
 
 def load_level(level):
+    """ Loading Tiledmap with pytmx """
+
+    global spawn_boxes
+
     level = pytmx.load_pygame('maps/level_' + level + '.tmx')
 
     y_num = 0
     for x, y, gid in level.get_layer_by_name('Objects'):
         if level.get_tile_image_by_gid(gid) != None:
-            matrix[y_num].append(0)
-        else:
             matrix[y_num].append(1)
+        else:
+            matrix[y_num].append(0)
         
         if x == 19: y_num += 1
+
+    spawn_boxes = []                                                # Areas in which enemies can spawn. Requires tiled type 'spawn_box'
+    for obj in level.get_layer_by_name('Triggers'):
+        if obj.type == 'spawn_box':
+            rect = pygame.rect.Rect(obj.x, obj.y, obj.width, obj.height)
+            if obj.name == 'north': 
+                rect = rect.move(0, -64)
+                rect.height += 64
+            if obj.name == 'east': 
+                rect = rect.move(64, 0)
+                rect.width += 64
+            if obj.name == 'south': 
+                rect = rect.move(0, 64)
+                rect.height += 64
+            if obj.name == 'west': 
+                rect = rect.move(-64, 0)
+                rect.width += 64
+            spawn_boxes.append(rect)
 
     return level
 
 level = load_level('1')
-grid = Grid(matrix=matrix)
-
-wolf_1 = enemies.Wolf(100, 220, grid)
-wolf_2 = enemies.Wolf(100, 420, grid)
-all_sprites.add(wolf_1)
-all_sprites.add(wolf_2)
-all_enemies.add(wolf_1)
-all_enemies.add(wolf_2)
 
 def draw():
     screen.blit(background, (0,0))
@@ -65,14 +78,15 @@ def draw():
 
     all_sprites.draw(screen)
 
-    for x, y, gid in level.get_layer_by_name('Objects'):
-        image = level.get_tile_image_by_gid(gid)
+    for x, y, gid in level.get_layer_by_name('Objects'):                        # Handles drawing of tile images.
+        image = level.get_tile_image_by_gid(gid)                                # Retrieves the image of a tile using it's 'gid'. IDK what a gid is tho.
         if image != None:
             screen.blit(image, (x * level.tilewidth, y * level.tileheight))
 
-    pygame.display.flip()
+    pygame.display.flip()                                                       # Refresh display
 
 def handle_event(event):
+    """ Returns True if the game has been closed. """
     if event.type == pygame.QUIT:
         return True
 
@@ -85,7 +99,9 @@ def distance(a, b):
 
     return math.sqrt(dx*dx + dy*dy)
 
-def fire():
+def fire_mouse():
+    """ Firing function that uses the mouse rather than the arrow keys. """
+
     global fireball_cooldown
 
     mouse_pos = pygame.mouse.get_pos()
@@ -99,9 +115,57 @@ def fire():
         new_bullet = projectiles.Fireball(player.rect.center[0], player.rect.center[1], angle)
         bullets.add(new_bullet)
 
+def fire_arrows():
+    """ Firing function that uses the arrow keys rather than the mouse. """
+
+    global fireball_cooldown
+
+    angle = None
+    if keys[pygame.K_UP]: 
+        angle = 270
+        if keys[pygame.K_LEFT]: angle = 225
+        elif keys[pygame.K_RIGHT]: angle = 315
+    elif keys[pygame.K_DOWN]: 
+        angle = 90
+        if keys[pygame.K_LEFT]: angle = 135
+        elif keys[pygame.K_RIGHT]: angle = 45
+    elif keys[pygame.K_LEFT]: angle = 180
+    elif keys[pygame.K_RIGHT]: angle = 0
+
+
+    if angle != None:
+        fireball_cooldown += 1
+
+        new_bullet = projectiles.Fireball(player.rect.center[0], player.rect.center[1], angle)
+        bullets.add(new_bullet)
+
+def spawn_enemies():
+    """ Spawns a random amount of enemies from a random spawning box. Currently non-functional due to rewrite"""
+
+    enemy_num = random.randint(1,5)
+    spawn_box = spawn_boxes[random.randint(0, 3)]
+
+    if spawn_box.y <= 0: start = [0, 128]
+    elif spawn_box.y >= 640: start = [0, -128]
+    elif spawn_box.x <= 0: start = [128, 0]
+    elif spawn_box.x >= 640: start = [-128, 0]
+
+    x = spawn_box.x
+    y = spawn_box.y
+    new_enemies = []
+    for i in range(enemy_num):
+        new_enemies.append(enemies.Wolf(x + 32, y + 32, grid, (x + 32 + start[0], y + 32 + start[1])))
+        x += 64
+        if not spawn_box.collidepoint(x, y):
+            x = spawn_box.x
+            y += 64
+
+    all_enemies.add(new_enemies)
+    all_sprites.add(new_enemies)
+
 done = False
 while not done:
-    keys = pygame.key.get_pressed()
+    keys = pygame.key.get_pressed()                     # Retrieves all currently pressed keys as dict
     for event in pygame.event.get():
         done = handle_event(event)
 
@@ -117,19 +181,18 @@ while not done:
             bullets.remove(bullet)
     
     for enemy in all_enemies:
-        enemy.cycle_anim()
+        move = enemy.move()
 
-        collide = False
-        for enemy_2 in all_enemies:
-            if enemy != enemy_2:
-                if enemy.rect.move(enemy.move(player.rect.center)).colliderect(enemy_2.rect):
-                    collide = True
-                    enemy.path = enemy.find_path(player.rect.center)
+        if move == True:                                             
+            matrix[enemy.path[0][0]][enemy.path[0][1]] = 0              # Resets the current and next (i.e the one the enemy has just reached) point in the path on the matrix
+            matrix[enemy.path[1][0]][enemy.path[1][1]] = 0
+            enemy.path = enemy.find_path(player.rect.center, matrix)
+            matrix[enemy.path[0][0]][enemy.path[0][1]] = 1              # Sets the current and next point in the path to obstacles on the matrix so enemies avoid each other
+            matrix[enemy.path[1][0]][enemy.path[1][1]] = 1
+        else:
+            enemy.rect.center += move
 
-        if not collide:
-            enemy.rect.center += enemy.move(player.rect.center)
-
-        for fireball in bullets:
+        for fireball in bullets:                                        # Destroying enemies
             if fireball.origin == 'plr':
                 if enemy.rect.colliderect(fireball.rect):
                     all_sprites.remove(enemy)
@@ -139,13 +202,16 @@ while not done:
     if fireball_cooldown >= max_cooldown:
         fireball_cooldown = 0
 
+    #if random.randint(1,100) == 69 and len(all_enemies.sprites()) < 10:
+    #    spawn_enemies()
+
     player.rect.center += player.move()
 
     if fireball_cooldown > 0:
-        fireball_cooldown += 1
+        fireball_cooldown += 1                                          # Increment cooldown until it hits max_cooldown
     else:
-        fire()
+        fire_arrows()
 
     draw()
 
-    tick += clock.tick(60)/1000
+    tick += clock.tick(60)/1000                                         # Increment tick every second
